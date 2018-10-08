bb354f3914947465d210b859042cfe35cc3df5cc
Teje87 <mikeltejero1987@gmail.com>
2018-10-08 21:43:28 +0200

    Pendiente solucionar prolemas con google auth

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const app = express();
+const keys = require("./config/keys");
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.GOOGLE_CLIENT_ID,
+        clientSecret: keys.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {console.log(accessToken)}
+    )
+);
 
 //ROUTES
+
+app.get(
+    '/auth/google', 
+    passport.authenticate(
+        'google',
+        { scope : ['profile','email']})
+)
+
+
 app.get('/', (req, res)=>{
     console.log('get request running')
     res.send({bye: 'goodbye'}) 
