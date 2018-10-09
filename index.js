0fa7efae574a2ac1150b53290c160269293f7c41
Teje87 <mikeltejero1987@gmail.com>
2018-10-09 16:55:47 +0200

    añadido nodemon, pendiente refactorizar en modulos

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.GOOGLE_CLIENT_ID,
-        clientSecret: keys.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
-    },
-    (accessToken) => {console.log(accessToken)}
+    new GoogleStrategy(
+        {
+            clientID: keys.GOOGLE_CLIENT_ID,
+            clientSecret: keys.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token: ' + accessToken);
+            console.log('refresh token: ' + refreshToken);
+            console.log('profile: ' + JSON.stringify(profile));
+            console.log('done: ' + done);
+        }
     )
 );
 
@@ -25,6 +31,8 @@ app.get(
         'google',
         { scope : ['profile','email']})
 )
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 
 app.get('/', (req, res)=>{
