0da625806a6b8e15a9d0440b53ad714956081e1b
Teje87 <mikeltejero1987@gmail.com>
2018-10-11 20:53:10 +0200

    Configurado modelo User, pendiente probar a crear registros en la colección user

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+    .then(console.log("CONNECTED"))
+    .catch(error => console.log(error));
+
+
+
 require('./routes/authRoutes')(app);
 
 
