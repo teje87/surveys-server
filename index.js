0034bee062a56ecb36fe67e43540c150a16ece50
Teje87 <mikeltejero1987@gmail.com>
2018-10-12 13:48:29 +0200

    Guardando registros en la colección users

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./services/passport');
 require('./models/User');
+require('./services/passport');
+
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
     .then(console.log("CONNECTED"))
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,11 @@
 const keys = require("../config/keys");
 var passport = require('passport');
+const mongoose = require('mongoose');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
+
+const User = mongoose.model('users')
 
 
 passport.use(
@@ -13,10 +16,16 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token: ' + accessToken);
-            console.log('refresh token: ' + refreshToken);
-            console.log('profile: ' + JSON.stringify(profile));
-            console.log('done: ' + done);
+            User.findOne({googleId : profile.id})
+                .then((existingUser)=> {
+                    if(existingUser){
+                        console.log("the user exists")
+                    }else{
+                        new User({googleId : profile.id})
+                        .save()
+                        .then(user=>console.log(user))
+                    }
+                })
         }
     )
 );
