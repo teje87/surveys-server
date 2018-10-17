e759ae1e6710e3c339bf938f6bc965bdba788fa8
Teje87 <mikeltejero1987@gmail.com>
2018-10-17 20:22:29 +0200

    Configurado dev y prod con las variables de entorno en heroku

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if(PROCESS.env.NODE_ENV === "production"){
+    //load production config file
+    module.exports = require('./prod')
+}else{
+    //load development config file
+    module.exports = require('./dev');
+}
